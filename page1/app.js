1f9bc723f515ceced10875f66a12d00aa4d3d703
Sarvesh Patil <sarvesh2902@gmail.com>
2022-03-07 20:00:53 +0530

    Added page 1 and 2

--- page1/app.js
+++ page1/app.js
@@ -1,22 +1,53 @@
 const countryList = document.querySelector('#country-list');
 const countries = countryList.getElementsByTagName('li');
-const searchBox = document.getElementById('#search-input');
+const searchBox = document.getElementById('search-input');
+let countryArray = [];
+let ul = document.querySelector('#country-list');
+
+searchBox.addEventListener('keyup', async (e) => {
+
+    const searchTerm = e.target.value;
+    const array = await getCountries();
+    const lower = array.map(element => {
+        return element.toLowerCase();
+    });
+    const filteredCountries = lower.filter((country) => {
+        return country.includes(searchTerm.toLowerCase());
+    });
+    const distinct = [...new Set(filteredCountries)];
+
+    ul.innerHTML = '';
+    for (country of distinct) {
+        const newLI = document.createElement("li");
+        newLI.textContent = country.toUpperCase();
+        countryList.append(newLI);
+    }
+});
 
 const getCountries = async () => {
-    const res = await axios.get('https://restcountries.com/v3.1/all');
-    const countries = res.data;
-    let countryArray = [];
-    for (let country of countries) {
-        countryArray.push(country.name.common);
+    try {
+        const res = await axios.get('https://restcountries.com/v3.1/all');
+        const countriesNames = res.data;
+        for (let country of countriesNames) {
+            countryArray.push(country.name.common);
+        }
+        const arr = Object.values(countryArray);
+        const lower = arr.map(element => {
+            return element.toLowerCase();
+        });
+        return lower.sort();
     }
-    return countryArray.sort();
+    catch (e) {
+        return e;
+    }
 }
 
 const makeLI = async () => {
     const countryNames = await getCountries();
+    window.demoName = countryNames;
     for (country of countryNames) {
         const newLI = document.createElement("li");
-        newLI.textContent = country;
+        newLI.textContent = country.toUpperCase();
         countryList.append(newLI);
     }
 }
@@ -27,25 +58,11 @@ const addClickListener = async () => {
 
     for (li of lis) {
         li.addEventListener('click', (e) => {
-            window.location = '/Users/sarveshpatil/Desktop/Sarvesh/Projects/WebProjects/Maps/page2/page2.html'
+            console.log(e.target.innerText);
+            window.location = 'page2.html'
             localStorage.setItem('country', e.target.innerText);
         });
     };
 }
 
 addClickListener();
-
-// let arr = Array.from(countries);
-
-// function updateResult(query) {
-//     let resultList = document.querySelector("#country-list");
-//     resultList.innerHTML = "";
-
-//     arr.map(function (algo) {
-//         query.split(" ").map(function (word) {
-//             if (algo.toLowerCase().indexOf(word.toLowerCase()) != -1) {
-//                 resultList.innerHTML += `<li class="list-group-item">${algo}</li>`;
-//             }
-//         })
-//     })
-// }
